1a314815d858d95081b2941c081f569da88a79f3
dulal71 <ahmeddulal4211@gmail.com>
2026-05-20 23:19:57 +0600

    add to rated section

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-slate-50">
+      <body className="min-h-full flex flex-col bg-white">
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
--- src/components/TopDoctor.jsx
+++ src/components/TopDoctor.jsx
@@ -1,14 +1,51 @@
-import { topDoctors } from "@/services/doctorData";
+"use client";
 
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import DoctorCard from "./DoctorCard";
 
-const TopDoctor =async () => {
-    const doctors= await topDoctors()
-    console.log(doctors);
-    return (
-        <div>
-            
+const TopDoctor = () => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/topDoctors");
+        const data = await res.json();
+        setDoctors(data);
+      } catch (error) {
+        console.log("Error fetching doctors:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="mx-auto space-y-6 flex flex-col justify-center items-center">
+        <div className="text-center space-y-5">
+         <h2 className="text-3xl font-bold md:text-5xl bg-linear-to-r from-blue-600 to-cyan-500 bg-clip-text text-transparent">Meet Our Top Specialists</h2>  
+         <p className="text-xl text-zinc-500">We bring you the most trusted healthcare professionals with excellent ratings and experience.</p> 
         </div>
-    );
+      <div className="mx-auto grid grid-cols-1 md:grid-cols-3 gap-5">
+        {doctors.map((doctor, index) => (
+          <motion.div
+            key={doctor._id}
+            initial={{ opacity: 0, x: 100 }}   // 👉 RIGHT → LEFT
+            whileInView={{ opacity: 1, x: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{
+              duration: 0.7,
+              delay: index * 0.2,
+              ease: [0.22, 1, 0.36, 1],
+            }}
+          >
+            <DoctorCard doctor={doctor} />
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopDoctor;
